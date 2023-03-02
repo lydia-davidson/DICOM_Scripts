698d63355d9b5059b6bea4889c7b06bf67cc6b7f
lydia-davidson <125041615+lydia-davidson@users.noreply.github.com>
2023-03-02 11:02:41 +0000

    Add files via upload

---
+++ SeriesDescription_Extractor.py
@@ -0,0 +1,22 @@
+import os
+import pydicom
+
+
+my_dir = r"D:\Lydia\SCHXR_sample"   # Paste the folder path to root folder you want to interogate
+
+
+f = open("SeriesDescriptors.txt", "w")    # Creates new text file where result will be saved
+
+for path, sub_dirs, files in os.walk(my_dir):  #Traverses directory recursively through any/all sub folders
+    for file in files:
+        if file.endswith(".dcm"):
+            file_path = os.path.join(path, file)  #Joins file to path statement
+            fn = pydicom.dcmread(file_path)
+            a = [file , fn.SeriesDescription]   #Output information want to display (file name and series descr tag)
+            print(a)
+            f.writelines("%s\n" % a)          #Saves results in text file
+
+
+
+
+
